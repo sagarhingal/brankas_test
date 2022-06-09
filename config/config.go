f503dad8f777ff5385f0a32694b8142e6f58578c
Sagar Hingal <sagar.hingal@gmail.com>
2022-06-09 12:40:08 +0530

    Added base code for loading configuration params from the yaml file which uses native go log package for printing logs and a simple http server.

---
+++ config/config.go
@@ -0,0 +1,42 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+// Config : A shared object for the service that
+// provides all the essential server data
+type Config struct {
+	Webserver struct {
+		Port string `yaml:"port"`
+	} `yaml:"webserver"`
+
+	Database struct {
+		Host     string `yaml:"host"`
+		Port     string `yaml:"port"`
+		DB       string `yaml:"db"`
+		Username string `yaml:"username"`
+	} `yaml:"database"`
+}
+
+var Configdata Config
+
+func Loadconfig(path string) (Config, error) {
+
+	configfile, err := os.Open(path)
+	if err != nil {
+		return Configdata, err
+	}
+	// Close the file after its use
+	defer configfile.Close()
+
+	decoder := yaml.NewDecoder(configfile)
+	err = decoder.Decode(&Configdata)
+
+	// Print the log after successfull loading of the config variables
+	log.Println("Loadconfig - ", Configdata)
+	return Configdata, err
+}
--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"brankas_test/config"
+	"log"
+	"net/http"
+)
 
 func main() {
 
-	fmt.Println("Hello World")
+	// Constants for the configuration
+	const (
+		configpath string = "env/config.yaml"
+	)
 
+	// Load configuration parameters
+	var configdata config.Config
+	configdata, err := config.Loadconfig(configpath)
+	if err != nil {
+		log.Fatalln("Loadconfig() - Unable to open the configuration file. | ", err)
+	}
+
+	// Start the server
+	log.Fatalln(http.ListenAndServe(":"+configdata.Webserver.Port, nil))
 }
